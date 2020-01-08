e4d0e17107ef30d9e8391a941fb9030f55dcc9a5
zy <varzy@live.com>
2020-01-08 17:48:32 +0800

    installed element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+// Vue
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import ElementUI from 'element-ui';
+// Styles
+import './styles/main.scss';
+
+Vue.use(ElementUI, { size: 'small' });
 
 Vue.config.productionTip = false;
 
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="index">hello, world</div>
+  <div class="index">
+    <p class="greeting">hello, world</p>
+    <el-button type="primary">Button</el-button>
+  </div>
 </template>
 
 <script>
@@ -7,3 +10,11 @@ export default {
   name: 'Index'
 }
 </script>
+
+<style lang="scss" scoped>
+.index {
+  .greeting {
+    color: $g-color-primary;
+  }
+}
+</style>
